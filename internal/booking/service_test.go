7d312de5e6174850fc8731a1dc61bd2e5d52923f
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-08-17 17:09:22 +0530

    test: add integration test for service endpoints

--- internal/booking/service_test.go
+++ internal/booking/service_test.go
@@ -229,3 +229,86 @@ func TestRoomsAvailableEndpointError_Unwrap(t *testing.T) {
 		t.Errorf("RoomsAvailableEndpointError.Unwrap() = %v, want %v", got, want)
 	}
 }
+
+func TestServiceEndpointsIntegration(t *testing.T) {
+	w := workplace.New()
+
+	_ = w.AddRoom("C-Cave", 3)
+	_ = w.AddRoom("D-Tower", 7)
+
+	w.AddBufferTime(newPeriodMust(workplace.NewPeriod(
+		newTimeMust(workplace.NewTime(9, 0)),
+		newTimeMust(workplace.NewTime(9, 15)),
+	)))
+
+	roomsAvailableEndpoint := MakeRoomsAvailableEndpoint(w)
+	bookRoomEndpoint := MakeBookRoomEndpoint(w)
+
+	var err error
+
+	// Test roomsAvailableEndpoint before booking
+	var roomsAvailableRequest, roomsAvailableResponse interface{}
+	wantRoomsAvailableResponse := RoomsAvailableResponse{
+		Vacancies: []workplace.Vacancy{{"C-Cave"}, {"D-Tower"}},
+	}
+
+	roomsAvailableRequest = RoomsAvailableRequest{
+		Period: newPeriodMust(workplace.NewPeriod(
+			newTimeMust(workplace.NewTime(10, 0)),
+			newTimeMust(workplace.NewTime(10, 30)),
+		)),
+	}
+
+	roomsAvailableResponse, err = roomsAvailableEndpoint(context.Background(), roomsAvailableRequest)
+	if err != nil {
+		t.Errorf("RoomsAvailable Endpoint() error = %v", err)
+	}
+
+	if !reflect.DeepEqual(roomsAvailableResponse, wantRoomsAvailableResponse) {
+		t.Errorf("RoomsAvailable Endpoint()= %v, want= %v", roomsAvailableResponse, wantRoomsAvailableResponse)
+	}
+
+	// Test bookRoomEndpoint
+
+	var bookRoomRequest, bookRoomResponse interface{}
+
+	bookRoomRequest = BookRoomRequest{
+		NumOfPeople: 2,
+		Period: newPeriodMust(workplace.NewPeriod(
+			newTimeMust(workplace.NewTime(10, 0)),
+			newTimeMust(workplace.NewTime(11, 0)),
+		)),
+	}
+
+	wantBookRoomResponse := BookRoomResponse{Reservation: workplace.Reservation{Room: "C-Cave"}, Err: nil}
+
+	bookRoomResponse, err = bookRoomEndpoint(context.Background(), bookRoomRequest)
+	if err != nil {
+		t.Errorf("BookRoom Endpoint() error = %v", err)
+	}
+
+	if !reflect.DeepEqual(bookRoomResponse, wantBookRoomResponse) {
+		t.Errorf("BookRoom Endpoint()= %v, want= %v", bookRoomResponse, wantBookRoomResponse)
+	}
+
+	// Test roomsAvailableEndpoint after booking
+	wantRoomsAvailableResponse = RoomsAvailableResponse{
+		Vacancies: []workplace.Vacancy{{"D-Tower"}},
+	}
+
+	roomsAvailableRequest = RoomsAvailableRequest{
+		Period: newPeriodMust(workplace.NewPeriod(
+			newTimeMust(workplace.NewTime(10, 0)),
+			newTimeMust(workplace.NewTime(10, 30)),
+		)),
+	}
+
+	roomsAvailableResponse, err = roomsAvailableEndpoint(context.Background(), roomsAvailableRequest)
+	if err != nil {
+		t.Errorf("RoomsAvailable Endpoint() error = %v", err)
+	}
+
+	if !reflect.DeepEqual(roomsAvailableResponse, wantRoomsAvailableResponse) {
+		t.Errorf("RoomsAvailable Endpoint()= %v, want= %v", roomsAvailableResponse, wantRoomsAvailableResponse)
+	}
+}
