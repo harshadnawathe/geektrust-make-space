1fc7aad36841e1f423a78ac7a15dd2572797548b
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-09-03 12:06:25 +0530

    add type InvalidRequestTypeError

--- internal/booking/service.go
+++ internal/booking/service.go
@@ -11,3 +11,24 @@ var (
 	ErrRoomsAvailableEndpointInvalidRequestType = fmt.Errorf("request type must be %T: %w", RoomsAvailableRequest{}, ErrInvalidRequestType)
 )
 
+type InvalidRequestTypeError struct {
+	Request, ExpectedType interface{}
+	Err                   error
+}
+
+func (err *InvalidRequestTypeError) Error() string {
+	return fmt.Sprintf(
+		"request type must be `%T`, given `%T`: %s",
+		err.ExpectedType,
+		err.Request,
+		ErrInvalidRequestType,
+	)
+}
+
+func (err *InvalidRequestTypeError) Unwrap() error {
+	return err.Err
+}
+
+func newInvalidRequestTypeError(req, exp interface{}) error {
+	return &InvalidRequestTypeError{req, exp, ErrInvalidRequestType}
+}
