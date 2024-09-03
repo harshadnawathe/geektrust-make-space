f98280dfde7a46b2315095dc11d99f3fd1fe4787
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-09-03 11:39:20 +0530

    refactor: move RoomsAvailable endpoint to separate file.

---
+++ internal/booking/RoomsAvailableEndpoint.go
@@ -0,0 +1,59 @@
+package booking
+
+import (
+	"context"
+	"fmt"
+	"geektrust/internal/workplace"
+)
+
+// RoomsAvailable service endpoint
+
+func MakeRoomsAvailableEndpoint(roomsAvailabler RoomsAvailabler) EndpointFunc {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		roomsAvailableRequest, err := getRoomsAvailableRequest(request)
+		if err != nil {
+			return nil, &RoomsAvailableEndpointError{request, err}
+		}
+
+		vacancies := roomsAvailabler.RoomsAvailable(
+			roomsAvailableRequest.Period,
+		)
+
+		return RoomsAvailableResponse{
+			Vacancies: vacancies,
+		}, nil
+	}
+}
+
+type RoomsAvailableRequest struct {
+	Period workplace.Period
+}
+
+type RoomsAvailableResponse struct {
+	Vacancies []workplace.Vacancy
+}
+
+type RoomsAvailabler interface {
+	RoomsAvailable(workplace.Period) []workplace.Vacancy
+}
+
+func getRoomsAvailableRequest(request interface{}) (RoomsAvailableRequest, error) {
+	roomsAvailableRequest, ok := request.(RoomsAvailableRequest)
+	if !ok {
+		return RoomsAvailableRequest{}, ErrRoomsAvailableEndpointInvalidRequestType
+	}
+	return roomsAvailableRequest, nil
+}
+
+type RoomsAvailableEndpointError struct {
+	Request interface{}
+	Err     error
+}
+
+func (err *RoomsAvailableEndpointError) Error() string {
+	return fmt.Sprintf("cannot handle request `%v`: %s", err.Request, err.Err)
+}
+
+func (err *RoomsAvailableEndpointError) Unwrap() error {
+	return err.Err
+}
--- internal/booking/service.go
+++ internal/booking/service.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"geektrust/internal/workplace"
 )
 
 var (
@@ -14,56 +13,3 @@ var (
 )
 
 type EndpointFunc func(context.Context, interface{}) (interface{}, error)
-
-
-// RoomsAvailable service endpoint
-
-type RoomsAvailableRequest struct {
-	Period workplace.Period
-}
-
-type RoomsAvailableResponse struct {
-	Vacancies []workplace.Vacancy
-}
-
-type RoomsAvailabler interface {
-	RoomsAvailable(workplace.Period) []workplace.Vacancy
-}
-
-func MakeRoomsAvailableEndpoint(roomsAvailabler RoomsAvailabler) EndpointFunc {
-	return func(_ context.Context, request interface{}) (interface{}, error) {
-		roomsAvailableRequest, err := getRoomsAvailableRequest(request)
-		if err != nil {
-			return nil, &RoomsAvailableEndpointError{request, err}
-		}
-
-		vacancies := roomsAvailabler.RoomsAvailable(
-			roomsAvailableRequest.Period,
-		)
-
-		return RoomsAvailableResponse{
-			Vacancies: vacancies,
-		}, nil
-	}
-}
-
-func getRoomsAvailableRequest(request interface{}) (RoomsAvailableRequest, error) {
-	roomsAvailableRequest, ok := request.(RoomsAvailableRequest)
-	if !ok {
-		return RoomsAvailableRequest{}, ErrRoomsAvailableEndpointInvalidRequestType
-	}
-	return roomsAvailableRequest, nil
-}
-
-type RoomsAvailableEndpointError struct {
-	Request interface{}
-	Err     error
-}
-
-func (err *RoomsAvailableEndpointError) Error() string {
-	return fmt.Sprintf("cannot handle request `%v`: %s", err.Request, err.Err)
-}
-
-func (err *RoomsAvailableEndpointError) Unwrap() error {
-	return err.Err
-}
